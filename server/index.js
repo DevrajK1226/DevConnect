abb35148c3e74db7bdfcc41f583809c029aafbd6
Devraj <devrajkanki@gmail.com>
2026-07-10 12:24:41 +0530

    Day 2: JWT auth - register/login routes with bcrypt password hashing

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
 require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,6 +14,7 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 // Test route
 app.get('/api/health', (req, res) => {
